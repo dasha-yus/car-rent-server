fb4996624721ec6a40ee868606fa87704effd259
Darya Yusipets <dyusipets@gomel.iba.by>
2022-06-29 14:43:26 +0300

    changed images and added route

--- src/routes/car.js
+++ src/routes/car.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Car = require("../models/car");
 
-router.get("/", (req, res) => {
+router.get("/cars", (req, res) => {
   Car.find()
     .then((videos) => res.json(videos))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.get("/cars/:id", (req, res) => {
+  Car.findById(req.params.id)
+    .then((car) => res.json(car))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
